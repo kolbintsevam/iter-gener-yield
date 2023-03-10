b009af7e30b82fa65da2021e901b25b4f8459867
Mariya Kolbintseva <kolbintsevam@mail.ru>
2023-03-10 16:42:37 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+import types
+
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.index = 0
+        self.index2 = -1
+        return self
+
+    def __next__(self):
+        self.index2 += 1
+        if self.index2 >= len(self.list_of_list[self.index]):
+            self.index += 1
+            self.index2 = 0
+        if self.index >= len(self.list_of_list):
+            raise StopIteration
+        return self.list_of_list[self.index][self.index2]
+
+
+def flat_generator(list_of_lists):
+    list_index = 0
+    while list_index != len(list_of_lists):
+        for i in list_of_lists[list_index]:
+            yield i
+        list_index += 1
+
+
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
